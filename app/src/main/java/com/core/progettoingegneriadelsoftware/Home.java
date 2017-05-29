bbb1d2c0a0f89734a94976a7b38cefdd6db3bea0
Niccolo <nikkom@ymail.com>
2017-05-29 12:27:54 +0200

    aa

--- app/src/main/java/com/core/progettoingegneriadelsoftware/Home.java
+++ app/src/main/java/com/core/progettoingegneriadelsoftware/Home.java
@@ -21,54 +21,45 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Dialog;
 
-import java.util.ArrayList;
-
 import application.MainApplication;
 import application.comunication.http.GetReceiver;
-import application.maps.components.Notify;
-import application.sharedstorage.Data;
-import application.sharedstorage.DataListener;
 import application.user.UserHandler;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener{
 
-    //menu laterale
+        //menu laterale
     private NavigationView navigationView;
-        //serve per eventuali errori durante il login
+        //elementi grafici dell'activity
     private AlertDialog alert;
-    private SharedPreferences prefer;
     private TextView tv;
+        //memorizza alcune informazioni nella memoria interna del dispositivo, per renderle persistenti
+    private SharedPreferences prefer;
+        //thread usato per la gestione delle notifiche inviate dal server
     private GetReceiver httpServerThread;
+        //contatore delle volte in cui si clicca il tasto onBack, utilizzato quando si vuole uscire dall'applicazione
     private int backpress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        //impostazione dei vari componenti grafici
+            //impostazioni riguardati i vari componenti grafici
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Go Safe!");
 
-
-
-        //thread che resta in ascolto per le notifiche
-        httpServerThread = new GetReceiver();
-        httpServerThread.start();
-
-
-
-
+            //barra laterale del menu
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
+            //oggetto alert, visualizzato eventualmente quando vengono immessi dati sbagliati duranti il login
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("")
                 .setCancelable(false)
@@ -78,18 +69,25 @@ public class Home extends AppCompatActivity
                     }
                 });
         alert = builder.create();
-
-        //vengono effettuate alcune configurazioni a livello di utente e si software
-
-        prefer = getApplicationContext().getSharedPreferences("SessionPref", 0); // 0 - for private mode
-        Editor edi = prefer.edit();
-
-        UserHandler.setPref(prefer);
-
+            //elemento grafico usato in alto nel menu (qui compare eventualmente il nome dell'utente, quando sarà effettuato il login
         View headerView = navigationView.inflateHeaderView(R.layout.nav_header_home);
         tv = (TextView)headerView.findViewById(R.id.text_logName);
 
+            //thread con il compito di ascoltare le notifiche provenienti dal server
+        httpServerThread = new GetReceiver();
+        httpServerThread.start();
+
+            //prefer permette l'accesso ai dati salvati nella memoria interna del dispositivo
+        prefer = getApplicationContext().getSharedPreferences("SessionPref", 0); // 0 - for private mode
+        Editor edi = prefer.edit();
+
+            //passata a UserHandler un'istanza delle preferencies, in modo che vengano assegnate
+            //eventuali informazioni li salvate, ad esso correlate (es. se già loggato vengono presi i suoi dati dalla memoria)
+        UserHandler.setPref(prefer);
+
             //prima controlla se è già loggato o se ci sono sharedpreferencies
+            //qualora il primo caso sia verificato viene scritto il nome dell'utente
+            //nel menu a tendina
         if(UserHandler.isLogged()) {
             tv.setText(UserHandler.getNome() + " " + UserHandler.getCognome());
         }
@@ -97,22 +95,24 @@ public class Home extends AppCompatActivity
             tv.setText("Utente non registrato");
         }
 
-
-
         backpress = 0;
-
+            //la creazione di quest'activity può avvenire in due circostanze, il cui discrimine è dato dal messaggio contenuto
+            //dentro extras
         Bundle extras = getIntent().getExtras();
+            //caso in cui l'activity sia creata per un'emergenza
         if(extras!=null) {
             if(extras.getString("MESSAGE").equals("EMERGENCY")) {
                 MainApplication.setEmergency(true);
                 finish();
             }
         }
+            //caso in cui l'activity è creata per il normale funzionamento dell'app
         else MainApplication.start(this);
     }
 
     protected void onStart() {
         super.onStart();
+            //impostati elementi presenti nel menu
         setOptionMenu();
         MainApplication.setCurrentActivity(this);
     }
@@ -136,34 +136,52 @@ public class Home extends AppCompatActivity
         super.onDestroy();
     }
 
-    @Override   //toglie il focus dal menu quando si clicca su altro layer
+    //toglie il focus dal menu quando si clicca su altro layer e gestisce l'uscita dall'applicazione
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            //se il menu è aperto si occupa di chiuderlo
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
-                backpress = (backpress + 1);
-                Toast.makeText(getApplicationContext(), " Press Back again to Exit ", Toast.LENGTH_SHORT).show();
+        }
+            //caso in cui si voglia uscire dall'applicazione
+        else {
+                //backpress fa in modo che si debba cliccare due volte il bottone per uscire
+            backpress = (backpress + 1);
+            Toast.makeText(getApplicationContext(), " Press Back again to Exit ", Toast.LENGTH_SHORT).show();
 
-                if (backpress>1) {
-                    Toast.makeText(getApplicationContext(), " Arrivederci ", Toast.LENGTH_SHORT).show();
+            if (backpress>1) {
+                Toast.makeText(getApplicationContext(), " Arrivederci ", Toast.LENGTH_SHORT).show();
 //                  TODO uscire spegnendo i receiver
+                if (MainApplication.getOnlineMode()) {
+                    MainApplication.closeApp(httpServerThread);
+                }
+                else {
+
                 }
             }
+        }
 
     }
 
+    /**
+     * Metodo per la gestione del login, da parte dell'utente (a questo livello solo per la gestione dell'interfaccia
+     * grafica)
+     */
     private void login() {
-        //crea un dialog per la form di login
+            //crea un dialog per la form di login
         final Dialog loginDialog = new Dialog(this);
         loginDialog.setContentView(R.layout.login_dialog);
         loginDialog.setTitle("Login");
-        //inizializzo componenti del dialog
+            //inizializzati i componenti del dialog
+            // i due bottoni
         Button btnCancel = (Button) loginDialog.findViewById(R.id.btnLoginCancel);
         Button btnLogin = (Button) loginDialog.findViewById(R.id.btnLoginEnter);
+            //elementi in cui scrivere
         final EditText txtUser = (EditText) loginDialog.findViewById(R.id.txtLoginUsername);
         final EditText txtPass = (EditText) loginDialog.findViewById(R.id.txtLoginPassword);
         final CheckBox chkLog = (CheckBox) loginDialog.findViewById(R.id.checkLog);
+            //gestione dell'evento di click
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -171,6 +189,7 @@ public class Home extends AppCompatActivity
                     //in base alla riuscita del login si cambiano i menu oppure si mostra alert
                 boolean b = UserHandler.login(txtUser.getText().toString(),txtPass.getText().toString(),chkLog.isChecked());
                 if (b) {
+                        //modificate le voci del menù
                     setOptionMenu();
 
                     Toast.makeText(getApplicationContext(),
@@ -192,22 +211,33 @@ public class Home extends AppCompatActivity
             }
         });
 
-        //rende visibile il dialog
+            //rende visibile il dialog
         loginDialog.show();
 
     }
 
-
+    /**
+     * Metodo per la gestione del logout da parte dell'utente: viene modificato lo UserHandler dell'applicazione
+     * e modificati gli elementi del menù
+     */
     private void logout() {
-        //richiama il metodo dello user per gestire i dati inerenti il logout
+        //richiama il metodo dello user per gestire i dati inerenti il logout dell'utente
         UserHandler.logout();
+            //cambiati gli elementi del menù
         setOptionMenu();
     }
 
-    //richiamato quando effettuo login e logout, imposta le voci del menu
+    /**
+     * Metodo che si occupa delle voci presenti all'interno del menu laterale, distinguendo i casi
+     * in cui l'utente abbia effettuato o meno il login (considerando che l'opzione ricerca delle mappe
+     * è presente in entrambe le situazioni
+     */
     private void setOptionMenu() {
+            //parte del menu in cui sono scritti nomi e cognome dell'utente
         tv = (TextView)findViewById(R.id.text_logName);
+            //caso in cui l'utente sia loggato: può modificare il profilo, effettuare la logout
         if(UserHandler.isLogged()) {
+                //impostate le voci del menu e le rispettive visibilità
             navigationView.getMenu().findItem(R.id.nav1).setTitle("Modifica Profilo");
             navigationView.getMenu().findItem(R.id.nav2).setTitle("Logout");
             navigationView.getMenu().findItem(R.id.view_information).setVisible(true);
@@ -218,12 +248,15 @@ public class Home extends AppCompatActivity
                 }
             }
         }
+            //caso in cui l'utente non sia ancora loggato: può entrare o iscriversi
         else {
             navigationView.getMenu().findItem(R.id.nav1).setTitle("Login");
             navigationView.getMenu().findItem(R.id.nav2).setTitle("Iscriviti");
             navigationView.getMenu().findItem(R.id.view_information).setVisible(false);
             if(tv!= null) tv.setText("utente non registrato");
         }
+            //in base alla modalità in cui lavora l'applicazione (online o meno)
+            //alcuni elementi del menu sono presenti o meno
         if(!MainApplication.getOnlineMode()){
             navigationView.getMenu().findItem(R.id.nav1).setEnabled(false);
             navigationView.getMenu().findItem(R.id.nav2).setEnabled(false);
@@ -231,52 +264,59 @@ public class Home extends AppCompatActivity
         }
     }
 
+        //metodo per la gestione del comportamento degli elementi presenti nel menu
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
 
         int id = item.getItemId();
-        if (UserHandler.isLogged()) {
-            switch(id){
-                case R.id.nav_maps:
-                //passa ad activity maps (per ora vuota)
-                Intent intentMap = new Intent (getApplicationContext(),
-                        ActivityMaps.class);
-                startActivity(intentMap);
-                    break;
-                case R.id.nav1 :
-                //modifica il profilo
-                Intent intentModify = new Intent (getApplicationContext(),
-                        InformationsHandler.class);
-                    intentModify.putExtra("editable", 1);
-                startActivity(intentModify);
-                    break;
-                case R.id.view_information:
-                    Intent intentView = new Intent (getApplicationContext(),
-                            InformationsHandler.class);
-                    intentView.putExtra("editable", 0);
-                    startActivity(intentView);
-                    break;
-                case R.id.nav2:
-                logout();
-                    break;
-            }
+            //caso in cui si vogliano visitare le mappe
+        if (id == R.id.nav_maps) {
+            Intent intentMap = new Intent (getApplicationContext(),
+                    ActivityMaps.class);
+            startActivity(intentMap);
         }
         else {
-            if (id == R.id.nav_maps) {
-                //passa ad activity maps (per ora vuota)
-                Intent intent = new Intent (getApplicationContext(),
-                        ActivityMaps.class);
-                startActivity(intent);
-
-            } else if (id == R.id.nav1) {
-                login();
-            } else if (id == R.id.nav2) {
-                Intent intent = new Intent (getApplicationContext(),
-                        InformationsHandler.class);
-                startActivity(intent);
+            if (UserHandler.isLogged()) {
+                switch(id){
+//                case R.id.nav_maps:
+//                //passa ad activity maps
+//                Intent intentMap = new Intent (getApplicationContext(),
+//                        ActivityMaps.class);
+//                startActivity(intentMap);
+//                    break;
+                    case R.id.nav1 :
+                            //modifica il profilo
+                        Intent intentModify = new Intent (getApplicationContext(),
+                                InformationsHandler.class);
+                        intentModify.putExtra("editable", 1);
+                        startActivity(intentModify);
+                        break;
+                            //caso in cui si vogliano vedere le informazioni dell'utente
+                    case R.id.view_information:
+                        Intent intentView = new Intent (getApplicationContext(),
+                                InformationsHandler.class);
+                        intentView.putExtra("editable", 0);
+                        startActivity(intentView);
+                        break;
+                    case R.id.nav2:
+                        logout();
+                        break;
+                }
+            }
+            else {
+                    //caso in cui si voglia effetuare il login
+                if (id == R.id.nav1) {
+                    login();
+                    //caso in cui si voglia registrare alla piattaforma
+                } else if (id == R.id.nav2) {
+                    Intent intent = new Intent (getApplicationContext(),
+                            InformationsHandler.class);
+                    startActivity(intent);
+                }
             }
         }
+
         /*if(id == R.id.viewuser){
             DatabaseUtility.viewColumn();
         }else if (id == R.id.delete){
